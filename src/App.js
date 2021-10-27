2dcca46b2d9a30f5c7b8d808c2f4d25a8eb1a693
Barun Mitra <itbarunmitra1000@gmail.com>
2021-10-27 16:33:24 +0600

    create footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Banner></Banner>
+      <Footer></Footer>
     </div>
   );
 }
